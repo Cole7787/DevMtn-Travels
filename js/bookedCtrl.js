7aaca48f67c65cdd0e712b4b2e6a10c432416f2c
Cole Allan <cacoleallan@gmail.com>
2016-12-19 16:22:15 -0700

    Fixed bug on the locations view

--- js/bookedCtrl.js
+++ js/bookedCtrl.js
@@ -1,13 +1,8 @@
 angular.module('devmtnTravel')
 .controller('bookedCtrl', function($scope, $state, mainSrv){
-console.log($state.params.id);
-
   for(var i = 0; i < mainSrv.travelInfo.length; i++){
     if($state.params.id == mainSrv.travelInfo[i].id){
       $scope.book = mainSrv.travelInfo[i];
     }
   }
-
-
-
 });
